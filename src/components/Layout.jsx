284293740857b548995b3809603ef3dac6e964a7
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2025-10-21 20:28:28 -0400

    add user login/logout logic

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react"
 import { Outlet } from "react-router-dom"
 import NavBar from "./NavBar"
 
 function Layout() {
+  // Add code to mock user authentication
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
+  const login = () =>{
+    setIsLoggedIn(true)
+  }
+
+  const logout = () =>{
+    setIsLoggedIn(false)
+  }
 
   return (
     <div className="layout">
-      <NavBar />
-      <Outlet />
+      { /* Pass logout as a prop to Navbar */}
+      <NavBar logout={logout} />
+        { /*Pass login function to Outlet as context */}
+      <Outlet context={login}/>
     </div>
   )
 }
