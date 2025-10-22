e316936e7f7c739b58db2aa3c4f923441b536498
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2025-10-21 20:55:14 -0400

    navigate to login if user not logged in

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react"
-import { Outlet } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { Outlet, useNavigate} from "react-router-dom"
 import NavBar from "./NavBar"
 
 function Layout() {
-  // Add code to mock user authentication
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  // call useNavigate and define navigate function 
+  const navigate = useNavigate()
 
   const login = () =>{
     setIsLoggedIn(true)
@@ -14,9 +15,19 @@ function Layout() {
     setIsLoggedIn(false)
   }
 
+    // Add programmatic navigation for login and logout
+  useEffect(() =>{
+    if (isLoggedIn) {
+        // navigates to Home route if user is logged in
+      navigate("/")
+    } else {
+        // navigates to Login route if user is logged out
+      navigate("/login")
+    }
+  }, [isLoggedIn])
+
   return (
     <div className="layout">
-      { /* Pass logout as a prop to Navbar */}
       <NavBar logout={logout} />
         { /*Pass login function to Outlet as context */}
       <Outlet context={login}/>
